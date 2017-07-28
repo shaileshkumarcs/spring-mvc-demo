7abd090268a416f4dc9baecdeb1a4e49e8bac02c
shaileshkumarcs <shailesh.kumarcs@gmail.com>
2017-07-28 10:08:29 +0530

    Added DAO Pakage and Enitiy class  changes

---
+++ src/com/shailesh/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,11 @@
+package com.shailesh.springdemo.dao;
+
+import java.util.List;
+
+import com.shailesh.springdemo.entity.Customer;
+
+public interface CustomerDAO {
+	
+	public List<Customer> getCustomers();
+
+}
